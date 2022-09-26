5feef32653d2bfa6004f6fff90b06fbd747f4832
HeligPfleigh <trantuan1892@gmail.com>
2022-09-26 22:42:52 +0700

    chore: encrypt other json and .env file

--- scripts/decrypt_secret.sh
+++ scripts/decrypt_secret.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 # Decrypt the file
-# mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
 --output ./android/app/google-services.json ./android/app/google-services.json.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output ./ios/GoogleService-Info.plist ./ios/GoogleService-Info.plist.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output ./.env ./.env.gpg
